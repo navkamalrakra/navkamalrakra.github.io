564b30a26272b9dccae87f6e6ca433157a08beb1
Navkamal Rakra <navkamal90@gmail.com>
2023-09-17 00:40:25 +0530

    chart inside function

--- script.js
+++ script.js
@@ -26,10 +26,32 @@ async function PopulateDataInArrays()
     }
   }
 
-PopulateDataInArrays().then(() => {});
+function CreateChart()
+{
+  return new Chart("myChart", {
+    type: "line",
+    data: {
+      labels: dateArr,
+      datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "rgba(0,0,255,0.1)",
+        data: preTaxArr
+      }]
+    },
+    options: {
+      legend: { display: true },
+      scales: {
+        yAxes: [{ ticks: { min: yValues.min(), max: yValues.max() } }],
+        responsive: true,
+        onResize: handleResize,
+        maintainAspectRatio: false
+      }
+    }
+  });
 
-const xValues = dateArr;
-const yValues = preTaxArr;
+}
 
 const handleResize = (myChart) => {
   myChart.resize();
@@ -43,25 +65,5 @@ Array.prototype.min = function () {
   return Math.min.apply(null, this);
 };
 
-new Chart("myChart", {
-  type: "line",
-  data: {
-    labels: xValues,
-    datasets: [{
-      fill: false,
-      lineTension: 0,
-      backgroundColor: "rgba(0,0,255,1.0)",
-      borderColor: "rgba(0,0,255,0.1)",
-      data: yValues
-    }]
-  },
-  options: {
-    legend: { display: true },
-    scales: {
-      yAxes: [{ ticks: { min: yValues.min(), max: yValues.max() } }],
-      responsive: true,
-      onResize: handleResize,
-      maintainAspectRatio: false
-    }
-  }
-});
+PopulateDataInArrays().then(() => {});
+CreateChart().then(() => {});
